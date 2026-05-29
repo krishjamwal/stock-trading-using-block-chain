41d75ce62a538a086f85a61bad25277df4378a89
krishjamwal <ajayjamwal401@gmail.com>
2026-05-29 22:28:37 +0530

    Update layout.jsx

--- client/src/components/layout.jsx
+++ client/src/components/layout.jsx
@@ -1,1 +1,17 @@
+import Sidebar from "./Sidebar"
 
+function Layout({ children }) {
+  return (
+    <div className="flex flex-col md:flex-row bg-gradient-to-br from-gray-900 to-gray-800 text-white min-h-screen">
+
+      <Sidebar />
+
+      <div className="flex-1 p-4 md:p-6">
+        {children}
+      </div>
+
+    </div>
+  )
+}
+
+export default Layout
