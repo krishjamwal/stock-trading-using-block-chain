25a699a2423261ac0f2b01516d16be2921daed09
krishjamwal <ajayjamwal401@gmail.com>
2026-06-02 21:40:58 +0530

    Update ProtectedRoutes.jsx

--- client/src/components/ProtectedRoutes.jsx
+++ client/src/components/ProtectedRoutes.jsx
@@ -1,1 +1,16 @@
+import { Navigate } from "react-router-dom"
 
+function ProtectedRoute({ children }) {
+
+  const token = localStorage.getItem("token")
+
+  // ❌ If not logged in → go to login
+  if (!token) {
+    return <Navigate to="/login" />
+  }
+
+  // ✅ If logged in → allow access
+  return children
+}
+
+export default ProtectedRoute
