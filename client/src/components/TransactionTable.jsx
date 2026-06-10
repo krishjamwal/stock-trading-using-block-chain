31dfe9cc8c23a2cf8a3d8eda760d40220da2a16c
krishjamwal <ajayjamwal401@gmail.com>
2026-06-10 23:17:32 +0530

    Update TransactionTable.jsx

--- client/src/components/TransactionTable.jsx
+++ client/src/components/TransactionTable.jsx
@@ -1,1 +1,38 @@
+const TransactionTable = ({ transactions }) => {
+  return (
+    <div className="bg-slate-800 p-5 rounded-xl mt-10">
+      <h2 className="text-2xl mb-5">Transactions</h2>
 
+      {
+        transactions.length === 0 ? (
+          <p>No transactions yet</p>
+        ) : (
+          <table className="w-full">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Qty</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {
+                transactions.map((t, i) => (
+                  <tr key={i}>
+                    <td>{t.name}</td>
+                    <td>${t.price}</td>
+                    <td>{t.quantity}</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        )
+      }
+
+    </div>
+  )
+}
+
+export default TransactionTable
