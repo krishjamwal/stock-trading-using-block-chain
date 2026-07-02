80a6d4e661cd0420880215b4ca1700b0c14e783b
krishjamwal <ajayjamwal401@gmail.com>
2026-07-02 23:04:10 +0530

    Update History.js

--- server/models/History.js
+++ server/models/History.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose")
 
+const historySchema = new mongoose.Schema({
+  value: Number,
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model("History", historySchema)
