5fc1fcd2e1e365bc38b7efec3b536d227a0b9a12
krishjamwal <ajayjamwal401@gmail.com>
2026-07-04 23:30:05 +0530

    Add Stock model with schema definition

--- server/models/Stock.js
+++ server/models/Stock.js
@@ -1,1 +1,25 @@
+const mongoose = require("mongoose")
 
+const stockSchema = new mongoose.Schema({
+  name: String,
+
+  price: Number,
+
+  quantity: Number,
+
+  userId: String,
+
+  buyPrice: {
+    type: Number,
+    default: 0
+  },
+
+  history: [
+    {
+      time: String,
+      price: Number
+    }
+  ]
+})
+
+module.exports = mongoose.model("Stock", stockSchema)
