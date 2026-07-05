a82beebf7a4eba077bbf79106fc69bf256cf27d2
krishjamwal <ajayjamwal401@gmail.com>
2026-07-05 22:40:02 +0530

    Add transaction schema for MongoDB model

--- server/models/Transctions.js
+++ server/models/Transctions.js
@@ -1,1 +1,15 @@
+const mongoose = require("mongoose")
 
+const transactionSchema = new mongoose.Schema({
+  userId: String,
+  stockName: String,
+  type: String, // buy or sell
+  quantity: Number,
+  price: Number,
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model("Transaction", transactionSchema)
