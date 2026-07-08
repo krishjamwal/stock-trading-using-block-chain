d55f8925a89eb7e2c19d116fdb43b010206eacb4
krishjamwal <ajayjamwal401@gmail.com>
2026-07-08 10:18:50 +0530

    Add Watchlist model with userId and name fields

--- server/models/Watchlist.js
+++ server/models/Watchlist.js
@@ -1,1 +1,8 @@
+const mongoose = require("mongoose")
 
+const watchlistSchema = new mongoose.Schema({
+  userId: String,
+  name: String
+})
+
+module.exports = mongoose.model("Watchlist", watchlistSchema)
